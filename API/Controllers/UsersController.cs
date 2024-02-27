52e0a5a9444afeb72ff7c538096e172409d8c7b1
Arturo Barajas <basaar.24@gmail.com>
2024-02-27 07:20:12 -0600

    Parcial02 - Added photo editor component

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -56,17 +56,31 @@ public class UsersController : BaseApiController
     public async Task<ActionResult<PhotoDto>> AddPhoto(IFormFile file)
     {
         var user = await _userRepository.GetUserByUsernameAsync(User.GetUsername());
+
         if (user == null) return NotFound();
+
         var result = await _photoService.AddPhotoAsync(file);
+
         if (result.Error != null) return BadRequest(result.Error.Message);
+
         var photo = new Photo
         {
             Url = result.SecureUrl.AbsoluteUri,
             PublicId = result.PublicId
         };
-        if(user.Photos.Count == 0) photo.IsMain = true;
+
+        if (user.Photos.Count == 0) photo.IsMain = true;
+
         user.Photos.Add(photo);
-        if (await _userRepository.SaveAllAsync()) return _mapper.Map<PhotoDto>(photo);
+
+        if (await _userRepository.SaveAllAsync())
+        {
+            return CreatedAtAction(
+                nameof(GetUser),
+                new { username = user.UserName },
+                _mapper.Map<PhotoDto>(photo));
+        }
+
         return BadRequest("Hubo un problema al subir tu foto");
     }
 }
