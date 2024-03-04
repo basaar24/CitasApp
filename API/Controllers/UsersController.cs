08fbe541e355f4f72e844c7332590b2a05601107
Arturo Barajas <basaar.24@gmail.com>
2024-03-04 07:04:14 -0600

    Parcial02 - Added support to set main photo

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -12,9 +12,12 @@ namespace API.Controllers;
 [Authorize]
 public class UsersController : BaseApiController
 {
+    #region Private vars and ctor
+
     private readonly IUserRepository _userRepository;
     private readonly IMapper _mapper;
     private readonly IPhotoService _photoService;
+
 
     public UsersController(IUserRepository userRepository, IMapper mapper,
         IPhotoService photoService)
@@ -23,6 +26,8 @@ public class UsersController : BaseApiController
         _mapper = mapper;
         _photoService = photoService;
     }
+
+    #endregion
 
     [HttpGet]
     public async Task<ActionResult<IEnumerable<MemberDto>>> GetUsers()
@@ -82,5 +87,24 @@ public class UsersController : BaseApiController
         }
 
         return BadRequest("Hubo un problema al subir tu foto");
+    }
+
+    [HttpPut("photo/{photoId}")]
+    public async Task<ActionResult> SetMainPhoto(int photoId)
+    {
+        var user = await _userRepository.GetUserByUsernameAsync(User.GetUsername());
+        if (user == null) return NotFound("No se encuentra el usuario");
+
+        var newMain = user.Photos.FirstOrDefault(photo => photo.Id == photoId);
+        if (newMain == null) return NotFound("No se encuentra la foto");
+        if (newMain.IsMain) return BadRequest("La foto ya es la principal");
+
+        var currentMain = user.Photos.FirstOrDefault(photo => photo.IsMain);
+        if (currentMain != null) currentMain.IsMain = false;
+        newMain.IsMain = true;
+
+        if (await _userRepository.SaveAllAsync()) return NoContent();
+
+        return BadRequest("No se pudo establecer la foto como principal");
     }
 }
