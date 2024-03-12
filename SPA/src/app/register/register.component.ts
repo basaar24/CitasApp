4d165a5829ffcd912918af56842e86c568d7004f
Arturo Barajas <basaar.24@gmail.com>
2024-03-12 07:55:51 -0600

    Parcial02 - Added a reusable date picker

--- SPA/src/app/register/register.component.ts
+++ SPA/src/app/register/register.component.ts
@@ -12,12 +12,14 @@ export class RegisterComponent implements OnInit {
   @Output() cancelRegister = new EventEmitter();
   model: any = {};
   registerForm: FormGroup = new FormGroup({});
+  maxDate: Date = new Date();
 
   constructor(private accountService: AccountService, private toastr: ToastrService,
     private fb: FormBuilder) { }
 
   ngOnInit(): void {
     this.initializeForm();
+    this.maxDate.setFullYear(this.maxDate.getFullYear() - 18);
   }
 
   initializeForm() {
@@ -31,7 +33,7 @@ export class RegisterComponent implements OnInit {
       password: ["", [Validators.required, Validators.minLength(4), Validators.maxLength(8)]],
       confirmPassword: ["", [Validators.required, this.matchValues("password")]],
     });
-    
+
     this.registerForm.controls["password"].valueChanges.subscribe({
       next: () => this.registerForm.controls["confirmPassword"].updateValueAndValidity(),
     });
